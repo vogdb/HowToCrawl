b6e0b0988957350a44e69da44e7001cec9c10b59
vogdb <vozhdb@gmail.com>
2014-05-30 13:03:25 +0600

    Unity project

---
+++ Assets/CrawlerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrawlerBehaviour : MonoBehaviour {
+
+	public float moveSpeed = .2f;
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+	void FixedUpdate () {
+		rigidbody2D.velocity = new Vector2(transform.localScale.x * moveSpeed, rigidbody2D.velocity.y);
+	}
+
+}
